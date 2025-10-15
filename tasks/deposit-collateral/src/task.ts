2f017bb41ea1eead4d51da2a8fae517c63f59d47
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2025-10-15 16:55:46 -0300

    Progress including test

--- tasks/deposit-collateral/src/task.ts
+++ tasks/deposit-collateral/src/task.ts
@@ -1,22 +1,44 @@
-import { ERC20Token, Transfer, CallBuilder, TokenAmount } from "@mimicprotocol/lib-ts";
+import {
+  ERC20Token,
+  Transfer,
+  CallBuilder,
+  TokenAmount,
+  Bytes,
+} from "@mimicprotocol/lib-ts";
 
 import { inputs } from "./types";
 import { ERC20 } from "./types/ERC20";
 
 export default function main(): void {
   const token = new ERC20(inputs.collateralToken, inputs.chainId);
-  const balance = token.balanceOf(inputs.smartAccount);
+  const tokenForFee = ERC20Token.fromAddress(
+    inputs.collateralToken,
+    inputs.chainId,
+  );
+  const balance = TokenAmount.fromBigInt(
+    tokenForFee,
+    token.balanceOf(inputs.smartAccount),
+  );
+  const threshold = TokenAmount.fromStringDecimal(
+    tokenForFee,
+    inputs.threshold,
+  );
 
-  if (balance.gt(inputs.threshold)) {
-    const toSupply = balance.sub(inputs.maxFee);
-    CallBuilder multiCall = CallBuilder.forChain(inputs.chainId).addUser(inputs.smartAccount).addMaxFee(TokenAmount.fromBigInt(token, inputs.maxFee));
-
-    Pconst approveCall = token.approve(inputs.morphoAddress, toSupply);
-
-  approveCall``
-
-  Transfer.create(token, inputs.amount, inputs.recipient, inputs.maxFee).send();
-
+  if (balance.gt(threshold)) {
+    const toSupply = balance.minus(threshold);
+    const maxFee = TokenAmount.fromStringDecimal(tokenForFee, inputs.maxFee);
+    const threshold = TokenAmount.fromStringDecimal(tokenForFee, inputs.maxFee);
+    const multiCall = CallBuilder.forChain(inputs.chainId)
+      .addUser(inputs.smartAccount)
+      .addMaxFee(maxFee);
+    const approveCall = token
+      .approve(inputs.morphoAddress, toSupply.amount)
+      .addMaxFee(maxFee)
+      .build().calls[0];
+    multiCall.addCall(
+      inputs.morphoAddress,
+      Bytes.fromHexString(approveCall.data),
+    );
+    multiCall.build().send();
   }
-
 }
--- tasks/deposit-collateral/tests/task.spec.ts
+++ tasks/deposit-collateral/tests/task.spec.ts
@@ -1,40 +1,73 @@
-import { runTask, Transfer } from '@mimicprotocol/test-ts'
-import { expect } from 'chai'
+import { runTask, Call } from "@mimicprotocol/test-ts";
+import { expect } from "chai";
 
-describe('Task', () => {
-  const taskDir = './'
+describe("Task", () => {
+  const taskDir = "./";
 
   const context = {
-    user: '0x756f45e3fa69347a9a973a725e3c98bc4db0b5a0',
-    settlers: [{ address: '0xdcf1d9d12a0488dfb70a8696f44d6d3bc303963d', chainId: 10 }],
+    user: "0x756f45e3fa69347a9a973a725e3c98bc4db0b5a0",
+    settlers: [
+      { address: "0xdcf1d9d12a0488dfb70a8696f44d6d3bc303963d", chainId: 10 },
+      { address: "0x609d831c0068844e11ef85a273c7f356212fd6d1", chainId: 42161 },
+    ],
     timestamp: Date.now(),
-  }
+  };
+
+  const buildCalls = (balance: string): ContractCallMock[] => [
+    {
+      request: {
+        to: inputs.collateralToken,
+        chainId: inputs.chainId,
+        data: "0x70a08231", // `balanceOf` fn selector
+      },
+      response: {
+        value: balance,
+        abiType: "uint256",
+      },
+    },
+    {
+      request: {
+        to: inputs.collateralToken,
+        chainId: inputs.chainId,
+        data: "0x313ce567", // `decimals` fn selector
+      },
+      response: {
+        value: "18",
+        abiType: "uint8",
+      },
+    },
+  ];
 
   const inputs = {
-    chainId: 10, // Optimism
-    token: '0x7f5c764cbc14f9669b88837ca1490cca17c31607', // USDC
-    amount: '1000000', // 1 USDC
-    recipient: '0xbce3248ede29116e4bd18416dcc2dfca668eeb84',
-    maxFee: '100000', // 0.1 USDC
-  }
+    chainId: 42161, // Arbitrum
+    collateralToken: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", // WETH
+    morphoAddress: "0x6c247b1F6182318877311737BaC0844bAa518F5e",
+    borrowMarket:
+      "0xca83d02be579485cc10945c9597a6141e772f1cf0e0aa28d09a327b6cbd8642c", // /weth-usdc,
+    smartAccount: "0xA15A054DD1ceB68945010c2D3f25FfB7aEc664eF",
+    threshold: "0.001", // 4 USD
+    maxFee: "0.0001", // ~0.40 USD
+  };
 
-  it('produces the expected intents', async () => {
-    const intents = (await runTask(taskDir, context, { inputs })) as Transfer[]
+  it("produces the expected intents", async () => {
+    const ethBalance = "1000000000000000000"; // 1 ETH
+    const calls = buildCalls(ethBalance);
+    const intents = (await runTask(taskDir, context, {
+      inputs,
+      calls,
+    })) as Call[];
 
-    expect(intents).to.be.an('array')
-    expect(intents).to.have.lengthOf(1)
+    expect(intents).to.be.an("array");
+    expect(intents).to.have.lengthOf(1);
 
-    expect(intents[0].type).to.be.equal('transfer')
-    expect(intents[0].settler).to.be.equal(context.settlers[0].address)
-    expect(intents[0].user).to.be.equal(context.user)
-    expect(intents[0].chainId).to.be.equal(inputs.chainId)
-    expect(intents[0].maxFees).to.have.lengthOf(1)
-    expect(intents[0].maxFees[0].token).to.be.equal(inputs.token)
-    expect(intents[0].maxFees[0].amount).to.be.equal(inputs.maxFee)
-
-    expect(intents[0].transfers).to.have.lengthOf(1)
-    expect(intents[0].transfers[0].token).to.be.equal(inputs.token)
-    expect(intents[0].transfers[0].amount).to.be.equal(inputs.amount)
-    expect(intents[0].transfers[0].recipient).to.be.equal(inputs.recipient)
-  })
-})
+    expect(intents[0].type).to.be.equal("call");
+    expect(intents[0].settler).to.be.equal(context.settlers[1].address);
+    expect(intents[0].user).to.be.equal(inputs.smartAccount.toLowerCase());
+    expect(intents[0].chainId).to.be.equal(inputs.chainId);
+    expect(intents[0].maxFees).to.have.lengthOf(1);
+    expect(intents[0].maxFees[0].token).to.be.equal(
+      inputs.collateralToken.toLowerCase(),
+    );
+    expect(intents[0].maxFees[0].amount).to.be.equal("100000000000000");
+  });
+});
